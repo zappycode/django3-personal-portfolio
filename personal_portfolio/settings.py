5088fe071525d0535944544c76a42fba81fe18be
ZappyCode <nick@zappycode.com>
2020-01-30 12:15:18 -0700

    Local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
